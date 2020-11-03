e89f483a92d7e6e30cba443f719d8809f0b99360
Szymon Drzewiecki <249478@student.pwr.edu.pl>
2020-11-03 12:03:02 +0100

    Usunieto zadania znajdujace sie poza katalogami

--- lab_01/archive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#wybralem opcje aby zamiast po masce, pliki byly wyszukiwane po rozszerzezeniu
-#argumenty nalezy zadac w nastepujacej kolejnosci: rozszerzenie (bez kropki, tzn
-#jpg zamiast .jpg), ilosc dni od ostatniej modyfikacji, nazwa archiwum(bez .tar)
-
-find -name "*.$1" -type f -mtime -$2 -exec tar -rvf "$3.tar" {} \;
